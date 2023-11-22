fe0ae35cb7fe43320901d13807f26564e016138a
elliottgdev <146915307+elliottgdev@users.noreply.github.com>
2023-11-22 16:09:21 +1100

    1.0
    might implement the other enemies in next update idk feeling lazy

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ var button = document.getElementById("attack")
 var magic = document.getElementById("magic")
 var heal = document.getElementById("heal")
 var flee = document.getElementById("flee")
-
-button.onclick = function(){Attack(0)}
-magic.onclick = function(){Attack(1)}
-heal.onclick = function(){Attack(2)}
-//flee.onclick = function(){Attack(3)}
+var shopHeals = document.getElementById("plus-heals")
+var shopStrength = document.getElementById("plus-strength")
 
 var healthPara = document.getElementById("health")
 var killPara = document.getElementById("kill-count")
 var enemyHealthPara = document.getElementById("enemy-health")
 var enemyDmgPara = document.getElementById("enemy-dmg")
+var healsPara = document.getElementById("heals")
+var moneyPara = document.getElementById("money")
 
 var enemyHealth = 30
 var enemyDmg = 15
@@ -21,6 +20,9 @@ var kills = 0
 var health = 100
 var baseDamage = 10
 var healAmount = 15
+var heals = 2
+var money = 10
+var canSpell = true
 
 var isDead = false
 
@@ -29,24 +31,36 @@ function Attack(type){
     switch(type){
         case (0):
             enemyHealth -= baseDamage
-            alert("you attack! you dealt " + baseDamage + " damage!")
+            alert("your warrior attacks dealing " + baseDamage + " damage!")
             break
         case (1):
-            enemyHealth -= baseDamage
-            alert("you cast a spell dealing " + baseDamage + " damage!")
+            enemyHealth -= baseDamage / 2
+            canSpell = false
+            alert("your mage cast a spell dealing " + baseDamage / 2 + " damage!")
             break
         case (2):
             health += healAmount
-            alert("you healed " + healAmount + " health!")
+            heals--
+            alert("your healer healed your team for " + healAmount + " health!")
+            if (health > 100){
+                health = 100
+            }
             break
         default:
             break
     }
 
+    if (!canSpell){
+        enemyHealth -= 3
+        alert("spell poison dealt 3")
+    }
+    
     if (enemyHealth <= 0){
         enemyHealth = Math.floor(Math.random() * 50) + 15
         enemyDmg = Math.floor(Math.random() * 20) + 4
         kills++
+        money += Math.floor(Math.random() * 15) + 5
+        canSpell = true
         UpdateText()
     }
     else {
@@ -63,6 +77,27 @@ function Attack(type){
     }
 }
 
+function Shop(value){
+    switch (value){
+        case (0):
+            if (money >= 10){
+                money -= 10
+                heals++
+            }
+            break
+        case (1):
+            if (money >= 30){
+                money -= 30
+                baseDamage += 4
+            }
+            break
+        default:
+            break
+    }
+
+    UpdateText()
+}
+
 function EnemyAttack(dmg){
     health -= dmg
     alert("enemy did " + dmg + " damage!")
@@ -74,11 +109,34 @@ function UpdateText(){
     healthPara.innerHTML = "health: " + health
     killPara.innerHTML = "kills: " + kills
     enemyDmgPara.innerHTML = "enemy damage: " + enemyDmg
+    moneyPara.innerHTML = "money: $" + money
+    healsPara.innerHTML = "heals: " + heals
 }
 
 function Tick(){
 
 }
+
+function UpdateEnemy(){
+    let enemySprite = document.getElementById("enemy-sprite")
+    enemySprite.src 
+    const enemySpriteArray = [
+    "images/enemies/eye.png",
+    "images/enemies/demon_goblin.png",
+    "images/enemies/goblin.png",
+    "images/enemies/lil_skelly.png",
+    "images/enemies/shroomy.png",
+    "images/enemies/skelly.png"] 
+    alert(enemySpriteArray)
+}
+
+button.onclick = function(){Attack(0)}
+if (canSpell) magic.onclick = function(){Attack(1)} 
+else magic.onclick = function(){alert("cant use that right now!")}
+heal.onclick = function(){Attack(2)}
+//flee.onclick = function(){Attack(3)}
+shopHeals.onclick = function(){Shop(0)}
+shopStrength.onclick = function(){Shop(1)}
 
 var tickTimer = setInterval(function(){Tick()}, 16.666667)
 UpdateText()
